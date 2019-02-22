f26be457ea234cb93220f2aa7ce879859d112f69
AL_ON_PC <alon.berrios@gmail.com>
2019-02-22 00:24:33 -0300

    [WIP] setting custom font

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Platform, View, Text } from 'react-native';
 import { Button, ThemeProvider, colors, Avatar, Card } from 'react-native-elements'
 import LifeCounter from "./components/LifeCounter"
+import { setCustomText } from "react-native-global-props"
+import { Font } from "expo"
 
 const theme = {
   Button: {
@@ -12,9 +14,6 @@ const theme = {
       height: 60
     }
   },
-  Card: {
-
-  },
   Image: {
     style: {
       width: 200,
@@ -23,11 +22,11 @@ const theme = {
     },
     // resizeMode: 'cover'
   },
-  // View: {
-  //   stlye: {
-  //     justifyContent: "center"
-  //   }
-  // },
+  Text: {
+    style: {
+      fontFamily: 'open-sans-bold'
+    }
+  },
   colors: {
     ...Platform.select({
       default: colors.platform.android,
@@ -36,21 +35,29 @@ const theme = {
   },
 };
 
+const customTextProps = {
+  style: {
+    fontFamily: 'Times New Roman'
+  }
+}
 
 export default class App extends React.Component {
+
+
   componentDidMount() {
     Expo.ScreenOrientation.allowAsync(Expo.ScreenOrientation.Orientation.LANDSCAPE_RIGHT);
+    Font.loadAsync({
+      'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+    });
   }
 
   componentWillUnmount() {
     Expo.ScreenOrientation.allowAsync(Expo.ScreenOrientation.Orientation.PORTRAIT);
   }
 
-
   render() {
     return (
       <ThemeProvider theme={theme}>
-        {/* <LifeCounter lifeTotal={this.currentLifeTotal(lifeTotal)} /> */}
         <LifeCounter></LifeCounter>
       </ThemeProvider>
     );
--- components/LifeCounter.js
+++ components/LifeCounter.js
@@ -1,68 +1,155 @@
 import React, { Component } from 'react'
 import { Card, Image, Button } from 'react-native-elements'
-import { View, Text, ActivityIndicator, StyleSheet } from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import { View, Text, ActivityIndicator, StyleSheet, ImageBackground } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { Col, Row, Grid } from "react-native-easy-grid";
+
+import { Font, AppLoading } from "expo"
 
 const imageUrl = `https://picsum.photos/200/200/?image=421`
+
+import FontAwesome
+
+
+    // from '../../node_modules/@expo/vector-icons/fonts/FontAwesome.ttf';
+    from "@expo/vector-icons/fonts/FontAwesome.ttf"
+
+
+import MaterialIcons
+
+
+    from '@expo/vector-icons/fonts/MaterialIcons.ttf';
 
 export default class LifeCounter extends Component {
 
     state = {
-        lifeTotal: 20
+        lifeTotal: 20,
+        playerName: 'Player 1',
+        // playerStatus: <Icon name="skull" size={100} color="white" />
+        playerStatus: undefined,
+        loading: true
     }
 
-    currentLifeTotal(life) {
-        return life;
-    }
-
-    handleLifeTotal = (lifeAdded) => {
+    handleLifeTotal = (lifeToChange) => {
         this.setState({
-            lifeTotal: this.state.lifeTotal + lifeAdded
+            lifeTotal: this.state.lifeTotal + lifeToChange
         })
     }
+
+    async componentDidMount() {
+        // await Font.loadAsync({
+        //     'open-sans-bold': require('../assets/fonts/OpenSans-Bold.ttf'),
+        // });
+        await Font.loadAsync({
+            FontAwesome,
+            MaterialIcons
+
+        });
+        this.setState({ loading: false });
+
+    }
+
     render() {
-        const AddLifeButton = (lifeToAdd) => lifeToAdd > 0 ?
-            (<Button icon={<Icon name="arrow-up" size={15} color="white"></Icon>} title={`+${lifeToAdd}`} onPress={() => this.handleLifeTotal(lifeToAdd)} ></Button>)
-            : (<Button icon={<Icon name="arrow-down" size={15} color="white"></Icon>} title={`${lifeToAdd}`} onPress={() => this.handleLifeTotal(lifeToAdd)} ></Button>)
+        const AddLifeButton = (lifeToChange) => lifeToChange > 0 ?
+            (<Icon raised name="menu-up" size={100} color='white' onPress={() => this.handleLifeTotal(lifeToChange)}></Icon>)
+            : (<Icon raised name="menu-down" size={100} color='white' onPress={() => this.handleLifeTotal(lifeToChange)}></Icon>)
 
-        const { lifeTotal } = this.state
+        const { lifeTotal, playerName, playerStatus } = this.state
 
-        return (
-            <View style={{
-                flex: 1,
-                flexDirection: 'column',
-                justifyContent: 'center',
-                alignItems: 'center',
-            }}>
+        if (this.state.loading) {
 
-                <Card title={`${lifeTotal}`} titleStyle={{ fontSize: 40, color: lifeTotal > 0 ? "green" : "red" }}>
-                    <View style={styles.buttonGrid}>
-                        {AddLifeButton(1)}
-                        {AddLifeButton(5)}
-                    </View>
-                    <View style={{ flexDirection: 'row' }}>
-                        <Image
-                            source={{ uri: imageUrl }}
-                            PlaceholderContent={<ActivityIndicator />}
-                        />
-                    </View>
-                    <View style={styles.buttonGrid}>
-                        {AddLifeButton(-1)}
-                        {AddLifeButton(-5)}
-                    </View>
-                </Card>
-            </View>
 
-        )
+            return (
+                <View>
+                    <ImageBackground source={require("../assets/background.jpg")} resizeMode="cover" style={{ width: '100%', height: '100%' }}>
+                        <Grid>
+                            <Col>
+                                <Grid>
+                                    <Row>
+                                        <Col style={styles.mtgLifeButton}>{AddLifeButton(1)}</Col>
+                                    </Row>
+                                    <Row>
+                                        <Col style={styles.mtgPlusOneLife}><Text style={styles.mtgLifeToChange}>1</Text></Col>
+                                    </Row>
+                                    <Row>
+                                        <Col style={styles.mtgLifeButton}>{AddLifeButton(-1)}</Col>
+                                    </Row>
+                                </Grid>
+                            </Col>
+
+                            <Col>
+                                <Grid>
+                                    <Row>
+                                        <Col style={styles.mtgLifeButton}><Text style={styles.mtgPlayerName}>{playerName}</Text></Col>
+                                    </Row>
+                                    <Row>
+                                        <Col style={styles.mtgLifeButton}><Text style={[styles.mtgLifeCounter, { color: lifeTotal > 0 ? "green" : "red" }]}>{lifeTotal}</Text></Col>
+                                    </Row>
+                                    <Row>
+                                        <Col style={styles.mtgLifeButton}><Text>{playerStatus}</Text></Col>
+                                    </Row>
+                                </Grid>
+                            </Col>
+
+                            <Col>
+                                <Grid>
+                                    <Row>
+                                        <Col style={styles.mtgLifeButton}>{AddLifeButton(5)}</Col>
+                                    </Row>
+                                    <Row>
+                                        <Col style={styles.mtgPlusFiveLife}><Text style={styles.mtgLifeToChange}>5</Text></Col>
+                                    </Row>
+                                    <Row>
+                                        <Col style={styles.mtgLifeButton}>{AddLifeButton(-5)}</Col>
+                                    </Row>
+                                </Grid>
+                            </Col>
+
+                        </Grid>
+                    </ImageBackground>
+                </View>
+            )
+        } else {
+            return (<AppLoading></AppLoading>)
+        }
     }
 }
 
 const styles = StyleSheet.create({
-    buttonGrid: {
-        flexDirection: 'row',
-        justifyContent: 'space-evenly',
-        alignItems: 'center',
-        flexWrap: 'wrap',
-        padding: 10
+    mtgBackground: {
+
     },
+    mtgLifeButton: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    mtgLifeToChange: {
+        fontSize: 100,
+        // color: 'white',
+    },
+    mtgPlusOneLife: {
+        flex: 1.5,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        // color: 'white'
+    },
+    mtgPlusFiveLife: {
+        flex: 1.5,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        // color: 'white'
+
+    },
+    mtgLifeCounter: {
+        fontSize: 150
+    },
+    mtgPlayerName: {
+        fontSize: 40,
+        fontWeight: 'bold',
+        // fontFamily: 'open-sans-bold',
+    }
 });
